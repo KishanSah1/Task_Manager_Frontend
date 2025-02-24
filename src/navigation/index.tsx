af8d4727090347a04b62b4b5c0135f46c981bd59
Kishan Sah <kishansah7086@gmail.com>
2025-02-24 12:13:13 +0530

    final working frontend with icons issue solved

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -13,6 +13,10 @@ import {ProfileScreen} from '../screens/ProfileScreen';
 import {TaskDetailScreen} from '../screens/TaskDetailScreen';
 import { Task } from './types';
 
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faHome, faList, faCheckCircle, faUser } from '@fortawesome/free-solid-svg-icons';
+
+
 // Explicitly define the param lists
 export type RootStackParamList = {
   Auth: undefined;
@@ -52,7 +56,29 @@ const AuthNavigator = () => (
 );
 
 const MainNavigator = () => (
-  <Tab.Navigator>
+  <Tab.Navigator
+      screenOptions={({ route }) => ({
+        headerShown: true,
+        tabBarIcon: ({ color, size }) => {
+          let icon;
+
+          if (route.name === 'Home') {
+            icon = faHome;
+          } else if (route.name === 'Categories') {
+            icon = faList;
+          } else if (route.name === 'CompletedTasks') {
+            icon = faCheckCircle;
+          } else if (route.name === 'Profile') {
+            icon = faUser;
+          }
+
+          return <FontAwesomeIcon icon={icon} size={24} color={color} />;
+        },
+        tabBarActiveTintColor: '#4c348b',
+
+        tabBarInactiveTintColor: 'black',
+      })}
+    >
     <Tab.Screen
       name="Home"
       component={HomeScreen}
@@ -107,31 +133,43 @@ export const Navigation = () => {
   );
 };
 
+export default Navigation;
+
 
 
 
 
 // import React from 'react';
-// import { View, StyleSheet } from 'react-native';
-// import { createStackNavigator } from '@react-navigation/stack';
-// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-// import { useSelector } from 'react-redux';
-// import { RootState } from '../store';
-// import { FAB } from 'react-native-paper';
+// import {createStackNavigator} from '@react-navigation/stack';
+// import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+// import {useSelector} from 'react-redux';
+// import {RootState} from '../store';
 
-// import { LoginScreen } from '../screens/LoginScreen';
-// import { RegisterScreen } from '../screens/RegisterScreen';
-// import { HomeScreen } from '../screens/HomeScreen';
-// import { CategoriesScreen } from '../screens/CategoriesScreen';
-// import { CompletedTasksScreen } from '../screens/CompletedTasksScreen';
-// import { ProfileScreen } from '../screens/ProfileScreen';
-// import { TaskDetailScreen } from '../screens/TaskDetailScreen';
-// import { Task } from './types';
+// import {LoginScreen} from '../screens/LoginScreen';
+// import {RegisterScreen} from '../screens/RegisterScreen';
+// import {HomeScreen} from '../screens/HomeScreen';
+// import {CategoriesScreen} from '../screens/CategoriesScreen';
+// import {CompletedTasksScreen} from '../screens/CompletedTasksScreen';
+// import {ProfileScreen} from '../screens/ProfileScreen';
+// import {TaskDetailScreen} from '../screens/TaskDetailScreen';
+// import {Task} from './types';
 
+// import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+
+// import {
+//   faHome,
+//   faFolder,
+//   faSync,
+//   faUser,
+// } from '@fortawesome/free-solid-svg-icons';
+
+
+
+// // Explicitly define the param lists
 // export type RootStackParamList = {
 //   Auth: undefined;
 //   Main: undefined;
-//   TaskDetail: { task?: Task };
+//   TaskDetail: {task?: Task};
 // };
 
 // export type AuthStackParamList = {
@@ -155,96 +193,71 @@ export const Navigation = () => {
 //     <AuthStack.Screen
 //       name="Login"
 //       component={LoginScreen}
-//       options={{ headerShown: false }}
+//       options={{headerShown: false}}
 //     />
 //     <AuthStack.Screen
 //       name="Register"
 //       component={RegisterScreen}
-//       options={{ headerShown: false }}
+//       options={{headerShown: false}}
 //     />
 //   </AuthStack.Navigator>
 // );
 
 // const MainNavigator = () => (
-//   <View style={{ flex: 1 }}>
-//     <Tab.Navigator>
-//       <Tab.Screen
-//         name="Home"
-//         component={HomeScreen}
-//         options={{
-//           headerShown: true,
-//           tabBarButton: (props) => (
-//             <FAB {...props} icon="home" style={styles.fabTab} />
-//           ),
-//         }}
-//       />
-//       <Tab.Screen
-//         name="Categories"
-//         component={CategoriesScreen}
-//         options={{
-//           headerShown: true,
-//           tabBarButton: (props) => (
-//             <FAB {...props} icon="format-list-bulleted" style={styles.fabTab} />
-//           ),
-//         }}
-//       />
-//       <Tab.Screen
-//         name="CompletedTasks"
-//         component={CompletedTasksScreen}
-//         options={{
-//           headerShown: true,
-//           tabBarButton: (props) => (
-//             <FAB {...props} icon="check-circle" style={styles.fabTab} />
-//           ),
-//         }}
-//       />
-//       <Tab.Screen
-//         name="Profile"
-//         component={ProfileScreen}
-//         options={{
-//           headerShown: true,
-//           tabBarButton: (props) => (
-//             <FAB {...props} icon="account" style={styles.fabTab} />
-//           ),
-//         }}
-//       />
-//     </Tab.Navigator>
-
-//     {/* Floating Action Button for Adding Tasks */}
-//     <FAB
-//       style={styles.fab}
-//       icon="plus"
-//       onPress={() => console.log('Add Task Pressed')}
-//     />
-//   </View>
+//   <Tab.Navigator
+//     screenOptions={({route}) => ({
+//       tabBarIcon: ({color, size}) => {
+//         let icon;
+//         if (route.name === 'Home') {
+//           icon = faHome;
+//         } else if (route.name === 'Categories') {
+//           icon = faFolder;
+//         } else if (route.name === 'CompletedTasks') {
+//           icon = faSync;
+//         } else if (route.name === 'Profile') {
+//           icon = faUser;
+//         }
+//         return <FontAwesomeIcon icon={icon} size={size} color={color} />;
+//       },
+//       tabBarActiveTintColor: '#007AFF',
+//       tabBarInactiveTintColor: 'gray',
+//     })}>
+//     <Tab.Screen name="Home" component={HomeScreen} />
+//     <Tab.Screen name="Categories" component={CategoriesScreen} />
+//     <Tab.Screen name="CompletedTasks" component={CompletedTasksScreen} />
+//     <Tab.Screen name="Profile" component={ProfileScreen} />
+//   </Tab.Navigator>
 // );
 
 // export const Navigation = () => {
-//   const isAuthenticated = useSelector((state: RootState) => state.auth.isAuthenticated);
+//   const isAuthenticated = useSelector(
+//     (state: RootState) => state.auth.isAuthenticated,
+//   );
 
 //   return (
 //     <Stack.Navigator>
 //       {!isAuthenticated ? (
-//         <Stack.Screen name="Auth" component={AuthNavigator} options={{ headerShown: false }} />
+//         <Stack.Screen
+//           name="Auth"
+//           component={AuthNavigator}
+//           options={{headerShown: false}}
+//         />
 //       ) : (
 //         <>
-//           <Stack.Screen name="Main" component={MainNavigator} options={{ headerShown: false }} />
-//           <Stack.Screen name="TaskDetail" component={TaskDetailScreen} options={{ title: 'Task Details' }} />
+//           <Stack.Screen
+//             name="Main"
+//             component={MainNavigator}
+//             options={{headerShown: false}}
+//           />
+//           <Stack.Screen
+//             name="TaskDetail"
+//             component={TaskDetailScreen}
+//             options={{title: 'Task Details'}}
+//           />
 //         </>
 //       )}
 //     </Stack.Navigator>
 //   );
 // };
 
-// const styles = StyleSheet.create({
-//   fabTab: {
-//     margin: 10,
-//     backgroundColor: '#6200ee',
-//   },
-//   fab: {
-//     position: 'absolute',
-//     right: 20,
-//     bottom: 80, // To position above the bottom tab
-//     backgroundColor: '#6200ee',
-//   },
-// });
+// export default Navigation;
